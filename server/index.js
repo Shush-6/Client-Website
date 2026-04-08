94fa2cafe7f9ffe6498447fffe7fc4548665e3c5
Suhawani Shukla <shuklasuhawani5@gmail.com>
2026-04-08 19:16:29 +0530

    CORS error

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3000"|| "",
+  origin: ['https://reikisite.netlify.app', 'http://localhost:3000'], 
+  methods: 'GET,POST,PUT,DELETE',
   credentials: true
 }));
 
