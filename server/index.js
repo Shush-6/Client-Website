cdd6edabc2713514121c205d2c33fafc838437f6
Suhawani Shukla <shuklasuhawani5@gmail.com>
2026-04-08 18:44:28 +0530

    backend modified

--- server/index.js
+++ server/index.js
@@ -9,8 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-   origin: "http://localhost:3000",
-  methods: ["GET", "POST"],
+  origin: "http://localhost:3000"|| "",
   credentials: true
 }));
 
@@ -50,7 +49,6 @@ app.get('/api/services', async (req, res) => {
   }
 });
 
-// POST contact form → save to DB + email Vibha
 app.post('/api/contact', async (req, res) => {
   const { name, email, phone, service, message } = req.body;
 
