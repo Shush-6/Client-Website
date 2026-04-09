bd19f8e24c9c0290088cc2ad3e5472c068e7886f
Suhawani Shukla <shuklasuhawani5@gmail.com>
2026-04-09 08:38:02 +0530

    email error fix

--- server/index.js
+++ server/index.js
@@ -29,13 +29,17 @@ const pool = new Pool({
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
-  secure: true, 
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS, 
   },
+  connectionTimeout: 10000, 
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
   tls: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false,
+    minVersion: "TLSv1.2"
   }
 });
 
